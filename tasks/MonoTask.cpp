bda5c0316fec2443a5ddf1a67366472b29c4637a
higor-melo <higor.melo@tidewise.io>
2025-06-12 17:29:26 -0300

    feat: allow maintaining the desired frame mode on a grayscaled image

--- tasks/MonoTask.cpp
+++ tasks/MonoTask.cpp
@@ -1,6 +1,7 @@
 /* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
 
 #include "MonoTask.hpp"
+#include <base-logging/Logging.hpp>
 
 using namespace image_preprocessing;
 using namespace base::samples::frame;
@@ -9,13 +10,12 @@ MonoTask::MonoTask(std::string const& name, TaskCore::TaskState initial_state)
     : MonoTaskBase(name, initial_state)
 {
     out_frame.reset(new Frame);
+    grayscale_frame.reset(new Frame);
 }
 
 MonoTask::~MonoTask()
 {
 }
-
-
 
 /// The following lines are template definitions for the various state machine
 // hooks defined by Orocos::RTT. See MonoTask.hpp for more detailed
@@ -30,9 +30,9 @@ MonoTask::~MonoTask()
 
 bool MonoTask::startHook()
 {
-    if (! MonoTaskBase::startHook())
+    if (!MonoTaskBase::startHook())
         return false;
-    
+
     frame_helper.setCalibrationParameter(_calibration.value());
     return true;
 }
@@ -41,18 +41,38 @@ void MonoTask::updateHook()
 {
     MonoTaskBase::updateHook();
 
-    //read all new frames
-    while(_frame.read(in_frame) == RTT::NewData)
-    {
-        //set bayer mode to the right bayer pattern if not specified 
-        if(frame_mode == MODE_BAYER && in_frame->isBayer())
+    // read all new frames
+    while (_frame.read(in_frame) == RTT::NewData) {
+        // set bayer mode to the right bayer pattern if not specified
+        if (frame_mode == MODE_BAYER && in_frame->isBayer())
             frame_mode = in_frame->getFrameMode();
 
         Frame* pout_frame = out_frame.write_access();
-        pout_frame->init((in_frame->getWidth()-offset_x)*scale_x,
-                (in_frame->getHeight()-offset_y)*scale_y,
-                in_frame->getDataDepth(),frame_mode,false);
-        frame_helper.convert(*in_frame,*pout_frame,offset_x,offset_y,resize_algorithm,calibrate);
+        pout_frame->init((in_frame->getWidth() - offset_x) * scale_x,
+            (in_frame->getHeight() - offset_y) * scale_y,
+            in_frame->getDataDepth(),
+            frame_mode,
+            false);
+
+        // Convert to Grayscale but keep the previous final mode
+        if ((_convert_to_grayscale.value()) && !(in_frame->isGrayscale()) &&
+            (frame_mode != MODE_GRAYSCALE)) {
+            Frame* pgrayscale_frame = grayscale_frame.write_access();
+            pgrayscale_frame->init(in_frame->getWidth(),
+                in_frame->getHeight(),
+                in_frame->getDataDepth(),
+                MODE_GRAYSCALE,
+                false);
+            frame_helper.convertColor(*in_frame, *pgrayscale_frame);
+            in_frame.reset(pgrayscale_frame);
+        }
+
+        frame_helper.convert(*in_frame,
+            *pout_frame,
+            offset_x,
+            offset_y,
+            resize_algorithm,
+            calibrate);
         out_frame.reset(pout_frame);
         _oframe.write(out_frame);
     }
@@ -70,4 +90,3 @@ void MonoTask::updateHook()
 // {
 //     MonoTaskBase::cleanupHook();
 // }
-
--- tasks/MonoTask.hpp
+++ tasks/MonoTask.hpp
@@ -11,8 +11,9 @@ namespace image_preprocessing {
 	friend class MonoTaskBase;
     protected:
         RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> in_frame;
+        RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> grayscale_frame;
         RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> out_frame;
-        
+
 
     public:
         MonoTask(std::string const& name = "image_preprocessing::MonoTask", TaskCore::TaskState initial_state = Stopped);
@@ -47,7 +48,7 @@ namespace image_preprocessing {
          *
          * The error(), exception() and fatal() calls, when called in this hook,
          * allow to get into the associated RunTimeError, Exception and
-         * FatalError states. 
+         * FatalError states.
          *
          * In the first case, updateHook() is still called, and recover() allows
          * you to go back into the Running state.  In the second case, the
